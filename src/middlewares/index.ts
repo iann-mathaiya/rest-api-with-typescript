f0c9d059798662083dca7ca3483e9e3c8ab7c39c
Ian Mathaiya <iannmathaiya@gmail.com>
2023-06-03 08:54:13 +0300

    setting up authenticated user login

---
+++ src/middlewares/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import {get, merge} from 'lodash'
+import { getUserBySessionToken } from '../db/users'
+
+export const isAuthenticated =async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies(['LOGIN-AUTH-COOKIE'])
+
+        if (!sessionToken) {
+            return res.sendStatus(403)
+        }
+
+        const existingUser = await getUserBySessionToken(sessionToken)
+
+        if (!existingUser) {
+            return res.sendStatus(403)
+        }
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(400)
+    }
+}
