48705f8f94f211533a3c8ac97caca36b3c78444a
Ian Mathaiya <iannmathaiya@gmail.com>
2023-06-03 09:35:44 +0300

    fetch all users only when authenticated

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,7 +4,7 @@ import { getUserBySessionToken } from '../db/users'
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const sessionToken = req.cookies(['LOGIN-AUTH-COOKIE'])
+        const sessionToken = req.cookies['LOGIN-AUTH-COOKIE']
 
         if (!sessionToken) {
             return res.sendStatus(403)
